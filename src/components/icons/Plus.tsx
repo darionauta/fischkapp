e30bdde72bfec729f25209f94b7343aec44f0966
darionauta <dariusz.stanek@gmail.com>
2023-08-08 21:38:55 +0200

    improved icon property names

--- src/components/icons/Plus.tsx
+++ src/components/icons/Plus.tsx
@@ -1,8 +1,8 @@
 export default function () {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-            <path d="M17 12H7" stroke="white" stroke-linecap="round"/>
-            <path d="M12 17V7" stroke="white" stroke-linecap="round"/>
+            <path d="M17 12H7" stroke="white" strokeLinecap="round"/>
+            <path d="M12 17V7" stroke="white" strokeLinecap="round"/>
         </svg>
     )
 }
