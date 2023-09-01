2e2b91032a11c80aa86657836ef082f62043099a
darionauta <dariusz.stanek@gmail.com>
2023-09-01 07:57:18 +0200

    improved flashcards display test

--- src/layouts/CardsList/__tests__/CardsList.spec.tsx
+++ src/layouts/CardsList/__tests__/CardsList.spec.tsx
@@ -1,21 +1,48 @@
-import { render, screen } from '@testing-library/react';
+import { describe, expect, vi } from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react';
+import { CardType } from '../../Card/types';
+import { CardsContextProvider } from '../../../context/CardsContext';
 import CardsList from '../CardsList';
-import Card from '../../Card/Card';
+
+const MOCK_CARDS = [
+    { _id: "asfasv", front: "Marko", back: "Polo" },
+    { _id: "sdczdc", front: "Marko", back: "Polo" },
+    { _id: "sdcsd6", front: "Marko", back: "Polo" },
+];
+
+function createFetchResponse(data: CardType[], status: boolean) {
+    return  {
+        json: () => new Promise((resolve) => resolve(data)),
+        ok: status
+    }
+}
+
 
 describe('Test displaying flashcards', () => {
-    it('should contain at least one child element', () => {
+    it('should contain list of cards', async () => {
         
-        const mockCards = [
-            { _id: "asfasv", front: "Marko", back: "Polo" },
-            { _id: "sdczdc", front: "Marko", back: "Polo" },
-            { _id: "sdcsd6", front: "Marko", back: "Polo" },
-        ];
-        
+        global.fetch = vi.fn().mockResolvedValue(createFetchResponse(MOCK_CARDS, true));
         render(
-            <CardsList>
-                { mockCards.map((card, index) => <Card key={index} card={mockCards[index]} />)}                
-            </CardsList>
+            <CardsContextProvider>
+                <CardsList />
+            </CardsContextProvider>
         );
-        expect(screen.getAllByText("Marko").length).toBe(3);
+        await waitFor(() => {
+            expect(screen.getAllByText("Marko").length).toBe(3);
+        });
+    });
+
+    it('should contain error information when is fetch error', async () => {
+        
+        global.fetch = vi.fn().mockResolvedValue(createFetchResponse(MOCK_CARDS, false));
+
+        render(
+            <CardsContextProvider>
+                <CardsList />
+            </CardsContextProvider>
+        );
+        await waitFor(() => {
+            expect(screen.getByText("Fetch error")).toBeInTheDocument();
+        });
     });
 });
